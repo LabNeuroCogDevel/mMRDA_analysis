d41f6f3923cc6e6457da6c9c7a6ace34fc6055a6
Will Foran <will.foran@gmail.com>
2014-03-24 14:22:02 -0400

    add motion to deconvolve, remove some polys

--- deconvolve.bash
+++ deconvolve.bash
@@ -2,8 +2,9 @@
 #
 # outline for running deconvolve
 # input:
-#   functional 
-#   block number (1-8, to find stimfile)
+#   1) functional 
+#   2) block number (1-8, to find stimfile)
+#   3) mcplots.par
 #
 # output:
 #  ????
@@ -29,28 +30,52 @@ done
 stimstart=$scriptdir/../behav/10672_20140318/SlotStims/${blk}_duration_start.1D
 stimspin=$scriptdir/../behav/10672_20140318/SlotStims/${blk}_duration_spin.1D
 
+# use mcplots ot make mcplots_censor.1D
+mcpar=$3
+[ -z "$3" -o ! -r "$3" ] && echo "cannot read motion '$mcpar'" && exit 1
+1d_tool.py -infile $mcpar -show_censor_count -censor_prev_TR -censor_motion 1 mcplots
+
+mcpardir=$(cd $(dirname $mcpar);pwd)
+motion1D="$mcpardir/3dvolreg.par" # output from 3dvolreg from MH's preprocessFunctional
+[ ! -r "$motion1D" ] && echo "cannot read motion censor file '$motion1D'" && exit 1
+
 #nTR=$(3dinfo -nv $func)
 #TR=1.5
 
-echo "$func($nTR) $blk $stimNOWIN"
+echo "$func($nTR) $blk"
+echo -e "start\t$stimstart\nanti\t$stimspin\nwin\t$stimWIN\nnowin\t$stimNOWIN"
 
 3dDeconvolve                                           \
       -input $func \
-      -polort 9     -tout                              \
-      -num_stimts 4                                    \
-      -stim_times_AM1 1 $stimstart 'dmBLOCK(1)' \
-      -stim_label 1 slotstart                               \
-      -stim_times_AM1 2 $stimspin 'dmBLOCK(1)' \
-      -stim_label     2 anticipation                       \
-      -stim_times 3 $stimWIN 'BLOCK(1,1)'    \
-      -stim_label 3 win                                \
-      -stim_times 4 $stimNOWIN 'BLOCK(1,1)' \
-      -stim_label 4 nowin                            \
-      -num_glt 4                                     \
-      -gltsym "SYM: win  -nowin"              -glt_label 1 wVn  \
-      -gltsym "SYM: anticipation -win"        -glt_label 2 aVw  \
-      -gltsym "SYM: anticipation -nowin"      -glt_label 3 aVn  \
-      -gltsym "SYM: anticipation -nowin -win" -glt_label 4 aVwn \
+      -polort 3     -tout                              \
+      -overwrite                                       \
+      -num_stimts 10                                    \
+      -stim_file 1 $motion1D'[0]' -stim_base 1 -stim_label 1 rx   \
+      -stim_file 2 $motion1D'[1]' -stim_base 2 -stim_label 2 ry   \
+      -stim_file 3 $motion1D'[2]' -stim_base 3 -stim_label 3 rz   \
+      -stim_file 4 $motion1D'[3]' -stim_base 4 -stim_label 4 mx   \
+      -stim_file 5 $motion1D'[4]' -stim_base 5 -stim_label 5 my   \
+      -stim_file 6 $motion1D'[5]' -stim_base 6 -stim_label 6 mz   \
+      \
+      -stim_times_AM1 7 $stimstart 'dmBLOCK(1)' \
+      -stim_label 7 slotstart                               \
+      -stim_times_AM1 8 $stimspin 'dmBLOCK(1)' \
+      -stim_label     8 anticipation                       \
+      -stim_times 9 $stimWIN 'BLOCK(1,1)'    \
+      -stim_label 9 win                                \
+      -stim_times 10 $stimNOWIN 'BLOCK(1,1)' \
+      -stim_label 10 nowin                            \
+      \
+      -num_glt 8                                     \
+      -censor  mcplots_censor.1D                     \
+      -gltsym "SYM: win  +nowin"              -glt_label 1 outcome  \
+      -gltsym "SYM: win  -nowin"              -glt_label 2 win-nowin  \
+      -gltsym "SYM: win"                      -glt_label 3 win  \
+      -gltsym "SYM: nowin"                    -glt_label 4 nowin  \
+      -gltsym "SYM: anticipation"             -glt_label 5 anticipation  \
+      -gltsym "SYM: anticipation -win"        -glt_label 6 anti-win  \
+      -gltsym "SYM: anticipation -nowin"      -glt_label 7 anti-nowin  \
+      -gltsym "SYM: anticipation -nowin -win" -glt_label 8 anti-outcome \
       -bucket intialcontrasts.nii.gz
 
       #-x1D stims/${ii}_X.xmat.1D   >  stims/${ii}.3ddlog 2>&1
@@ -60,3 +85,4 @@ echo "$func($nTR) $blk $stimNOWIN"
       #-stim_times 2 stims/${ii}_stimes_*_win.1D 'BLOCK(1.5,1)'     \
       # -stim_times 3 stims/${ii}_stimes_*nowin 'BLOCK(1.5,1)'    \
 
+1d_tool.py -cormat_cutoff 0 -show_cormat_warnings -infile intialcontrasts.xmat.1D | tee initialcontrasts.colin.txt
